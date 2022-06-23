99b0bb8e56daec774a7622b40b6c9b3bc7d6e140
Rinn Mattox <rinnreed@gmail.com>
2022-06-22 20:27:34 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Write your JavaScript code here.
+function init() {
+    const takeoff = document.getElementById("takeoff");
+    //const landing = document.getElementById("landing");
+    //const abortMission = document.getElementById("abortMission");
+
+    takeoff.addEventListener('click', () => {
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (result = true) {
+            document.getElementById("flightStatus").innerHTML = "Shuttle in Flight.";
+            //paragraph.setAttribute("flightStatus", "Shuttle in Flight.");
+          //  shuttleBackground.style.backgroundColor = "blue"
+          console.log(paragraph.innerHTML);// = "Space shuttle ready for takeoff";
+        }
+        
+    })
+}
+
 // Remember to pay attention to page loading!
+window.addEventListener("load", init); 
